6b4d353d70c6b3d106d8c1148ad9a907ee3bade5
Michael Wright <mkwright@gmail.com>
2016-09-13 18:01:19 -0600

    Adjusted the application with dependencies for jain, junit 5 (M1), Assertj

--- src/test/java/com/alianza/sip/AppTest.java
+++ src/test/java/com/alianza/sip/AppTest.java
@@ -1,38 +1,12 @@
 package com.alianza.sip;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.assertj.core.api.Assertions.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.jupiter.api.Test;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest  {
+  @Test
+  public void sampleTest() {
+    assertThat(1+1).isEqualTo(2);
+  }
 }
